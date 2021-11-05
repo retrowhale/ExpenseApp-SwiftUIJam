eca7872a522d01d7b08bdf77b799d0a2bea42dd9
retrowhale <89171572+retrowhale@users.noreply.github.com>
2021-11-05 12:33:58 -0700

    Initial Commit

---
+++ ExpenseApp-SwiftUIJam/ExpenseApp_SwiftUIJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp_SwiftUIJamApp.swift
+//  ExpenseApp-SwiftUIJam
+//
+//  Created by Deniz Donmezer on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp_SwiftUIJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
